04dfb3b63ffd9e5641f5b1f2decac61dbd782796
Jennifer Jimenez <jenniferjimenez90@gmail.com>
2022-01-20 11:33:55 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    (e).preventDefault();
+    handleNewTask(e.target.new_task_description.value);
+    form.reset();
+  })
 });
+function handleNewTask(list){
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click',handleDelete)
+  btn.textContent = "x"
+  li.textContent = `${list} `;
+  li.appendChild(btn);
+  console.log(li);
+  document.querySelector("#tasks").appendChild(li);
+}
+
+function handleDelete(e) {
+e.target.parentNode.remove();
+}
